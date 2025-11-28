32fa7a01be05e1fd49ac7f176664ee5bfc81cbc9
kripalsinghuk <kirpalsinghuk@gmail.com>
2025-11-29 01:08:46 +0530

    first commit

---
+++ lib/expenses.dart
@@ -0,0 +1,35 @@
+import 'package:expense_tracer/models/expese.dart';
+import 'package:flutter/material.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+  @override
+  State<Expenses> createState() {
+    return _ExpensesState();
+  }
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: "Burger & cold coffie",
+      amount: 100.00,
+      date: DateTime.now(),
+      category: Category.food,
+    ),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text('The Chart'),
+          ListView.builder(
+            itemBuilder: (context, index) =>
+                Text(_registeredExpenses[index].id),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:expense_tracer/expenses.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Expenses(),
+  ));
+}
+
