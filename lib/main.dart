b4d771b4f9acd12da8727af3a9c4f124471bdf3e
kripalsinghuk <kirpalsinghuk@gmail.com>
2025-12-02 15:54:35 +0530

    using dark mod

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,33 @@ import 'package:flutter/material.dart';
 var kColorScheme = ColorScheme.fromSeed(
   seedColor: Color.fromARGB(255, 96, 59, 181),
 );
+
+var kDarkColorScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: Color.fromARGB(255, 5, 99, 125),
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(horizontal: 18, vertical: 8),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+        // textTheme: ThemeData().textTheme.copyWith(
+        //   titleLarge: TextStyle(
+        //     fontWeight: FontWeight.normal,
+        //     color: kDarkColorScheme.onSecondaryContainer,
+        //     fontSize: 14,
+        //   ),
+        // ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -30,9 +54,8 @@ void main() {
             fontSize: 14,
           ),
         ),
-        
       ),
-
+      themeMode: ThemeMode.dark,
       home: Expenses(),
     ),
   );
